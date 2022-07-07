14f591f4ec84ae24e8727188a76a9b962aa88368
Xavieroffcial <108806813+Xavieroffcial@users.noreply.github.com>
2022-07-07 09:54:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 //or copy the codes/script,
 //pls give credit
 //no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
+//© 2022 𝖃𝖆𝖛𝖎𝖊𝖗-𝕭𝖔𝖙𝖟 Inc. 𝖃𝖆𝖛𝖎𝖊𝖗 - 𝕸𝕯
 //Thank you to Lord Buddha, Family and Myself
 //════════════════════════════//
 //recode kar ke youtube pe upload kar rhe ya
@@ -41,26 +41,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['ur number'] //ur owner number
-global.ownername = "ur name"
+global.owner = ['6283862006229'] //ur owner number
+global.ownername = "𝖃𝖆𝖛𝖎𝖊𝖗"
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bombdy
-global.botname = "ur bot name" //ur bot name
-global.websitex = "https://youtu.be/xn9RatOrbuI" //ur website
+global.botname = "𝖃𝖆𝖛𝖎𝖊𝖗-𝕸𝕯" //ur bot name
+global.websitex = "https://instagram.com/skymoonn_real?igshid=YmMyMTA2M2Y=" //ur website
 global.vidmenu = { url: 'https://a.uguu.se/zlzQZvXH.mp4' } //gif and video menu
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
-global.author = "🦄Dream Guy Xeon"
+global.packname = "𝖃𝖆𝖛𝖎𝖊𝖗-𝕸𝕯 Stickers\n\n\n\n\n\n\n🦄🦄🦄 "
+global.author = "🦄Dream Guy 𝖃𝖆𝖛𝖎𝖊𝖗"
 global.themeemoji = "🐶"
 global.reactmoji = "😘"
-global.ownertag = ['ur number'] //ur owner tag
-global.ownernummenu = ['ur number'] //ur owner number in menu and all
-global.watermark = "Xeon Bot Inc." //ur watermark
+global.ownertag = ['6283862006229'] //ur owner tag
+global.ownernummenu = ['6283862006229'] //ur owner number in menu and all
+global.watermark = "𝖃𝖆𝖛𝖎𝖊𝖗 𝕭𝖔𝖙𝖟 Inc." //ur watermark
 global.botscript = "https://github.com/DGXeon/CheemsBot-MD3"
-global.linkz1 = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS"
-global.linkz2 = "https://chat.whatsapp.com/EcycNbJFCVT5ZsG9xIGkqd"
+global.linkz1 = "https://chat.whatsapp.com/GLj5hBI2NQm5ExcMGaa4Mu"
+global.linkz2 = "https://chat.whatsapp.com/GLj5hBI2NQm5ExcMGaa4Mu"
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -89,7 +89,7 @@ global.autorep = []
 global.ntilink = []
 
 //omther
-global.lolhuman = "KaysaS"
+global.lolhuman = "Xavier"
 global.sessionName = "session"
 global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
